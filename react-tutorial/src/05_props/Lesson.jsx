1b0441cf74159cac501ebbecb59116800430916d
Dmitry Kovalevsky <dimitr0960@inbox.ru>
2019-08-08 19:38:15 +0300

    master: 05 props validation Element, Array, Object

---
+++ react-tutorial/src/05_props/Lesson.jsx
@@ -0,0 +1,93 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+// // PropTypes
+//
+// // Node
+// Component.propTypes = {
+// 	node: PropTypes.node,
+// }
+//
+// // Element
+// Component.propTypes = {
+// 	elem: PropTypes.element,
+// }
+//
+// // Instance Of
+// Component.propTypes = {
+// 	instance: PropTypes.instanceOf(Constructor),
+// }
+//
+// // One of
+// Component.propTypes = {
+// 	elem: PropTypes.oneOf(['val1', 'val2']),
+// }
+//
+// // One of type
+// Component.propTypes = {
+// 	elem: PropTypes.oneOfType([
+// 		PropTypes.string,
+// 		PropTypes.number,
+// 	]),
+// }
+//
+// // Array of / Object of
+// Component.propTypes = {
+// 	array: PropTypes.arrayOf(PropTypes.string),
+// 	object: PropTypes.objectOf(PropTypes.number),
+// }
+//
+// // Shape
+// Component.propTypes = {
+// 	obj: PropTypes.shape({
+// 		color: PropTypes.string,
+// 		fontSize: PropTypes.number,
+// 		lineHeight: PropTypes.number,
+// 	})
+// }
+
+export const Counter = ({ counter = 0 }) => {
+	return <h1>{`Counter component. Counter value is: ${counter}`}</h1>
+}
+
+export const Button = () => {
+	return <button>Simple button</button>
+}
+
+export class Lesson extends Component {
+	static propTypes = {
+		children: PropTypes.element,
+	}
+
+	static defaultProps = {
+		children: null,
+	}
+
+	state = {
+		counter: 0,
+	}
+
+	handleClick = () => {
+		this.setState(({counter}) => ({
+			counter: ++counter,
+		}))
+	}
+
+	render() {
+		const {counter} = this.state;
+		const {children, child} = this.props;
+
+		return(
+			<div>
+				{child}
+				<div>{counter}</div>
+				{children}
+				{/*{React.cloneElement(children, {counter: this.state.counter})}*/}
+				<button onClick={this.handleClick}>+1</button>
+			</div>
+		);
+	}
+}
+
+// export default Lesson;
+
--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Lesson from './04_props/Lesson.jsx';
+import {Lesson, Counter, Button} from "./05_props/Lesson";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Lesson />, document.getElementById('root'));
+ReactDOM.render(<Lesson child={<Button/>}><Counter/></Lesson>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
