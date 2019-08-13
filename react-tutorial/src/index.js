9c3809957fbf9bbe6f24fb9b599b2173cd78599b
Dmitry Kovalevsky <dimitr0960@inbox.ru>
2019-08-13 09:57:35 +0300

    08_lists_keys

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Lesson from "./07_rendering/Lesson";
+import Lesson from "./08_lists_keys/Lesson";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Lesson />, document.getElementById('root'));
