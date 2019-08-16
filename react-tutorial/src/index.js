d4f24bde9c536142737af59c73846d46c5e13197
Dmitry Kovalevsky <dimitr0960@inbox.ru>
2019-08-16 09:41:48 +0300

    10_forms

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Lesson from "./09_fragments_css/Lesson";
+import Lesson from "./10_forms/Lesson";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Lesson />, document.getElementById('root'));
